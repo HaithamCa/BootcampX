f55be7c85e92869ddfca89dd11d4987812875efa
hytham defrawy <hythamdefrawy@gmail.com>
2021-11-25 15:12:06 -0500

    Add some important changes to keep my secret

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,23 +1,32 @@
+require('dotenv').config()
+
+
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'wlxkecza',
-  password: 'wj3dz-4yxuwfkdBnR4LsDq122hO2TYeh', // '123'
-  host: 'castor.db.elephantsql.com', //'localhost'
-  database: 'wlxkecza', // BootcampX
-  port: 5432
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS, 
+  host: process.env.DB_HOST, 
+  database: process.env.DB_NAME, 
+  port: process.env.DB_PORT
 });
 
 // const verb = process.argv[2];
 const verb1 = process.argv[3];
 
-pool.query(`
+const queryString = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${verb1 || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,25 +1,30 @@
+require('dotenv').config()
+
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'wlxkecza',
-  password: 'wj3dz-4yxuwfkdBnR4LsDq122hO2TYeh', // '123'
-  host: 'castor.db.elephantsql.com', //'localhost'
-  database: 'wlxkecza', // BootcampX
-  port: 5432
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS, 
+    host: process.env.DB_HOST, 
+    database: process.env.DB_NAME, 
+    port: process.env.DB_PORT
 });
 
 const verb = process.argv[2];
 // const verb1 = process.argv[3];
-
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+`;
+
+const cohortName = [ process.argv[2] || 'JUL02' ];
+
+pool.query(queryString, cohortName)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
